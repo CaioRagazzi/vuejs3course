397764f862f9238b36fa8f5cd9905c45078a9ed6
Caio Ragazzi <ca.ragazzi@gmail.com>
2022-03-01 17:44:30 +0000

    section 7

---
+++ sec7/vue-project/src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ showDetailsButtonCaption }}</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "012 456 799",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  computed:{
+      showDetailsButtonCaption(){
+          return this.detailsAreVisible ? 'Hide' : 'Show'
+      }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
